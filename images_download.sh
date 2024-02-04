0627d8eef26b55739245fb7f82ea8ea00fb7b41b
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2024-02-04 18:15:11 +0100

    version 0.2

--- images_download.sh
+++ images_download.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -eq 0 ]
 then
     echo "You have to provide only a phrase to be searched for!"
+    exit 1
+elif [ $# -gt 2 ]
+then
+    echo "You have to provide two arguments tops!"
     exit 1
 fi
 
 phrase="$1"
+if [ -z $2 ]
+then
+    name="default"
+else
+    name="$2"
+fi
+
 url="https://free-images-api.p.rapidapi.com/images/$phrase" 
 touch links.txt
+count=0
 while read link
 do
     if [ $link != "null" ]
     then
-        echo $link >> links.txt
+        #echo $link >> links.txt
+        wget -b -c -O "$name$count.jpg" $link
+        ((count++))
     fi
 
 done< <(curl --request GET \
@@ -22,8 +36,7 @@ done< <(curl --request GET \
 	--header 'X-RapidAPI-Key: 017fa9b49cmsh1614af302384761p19fed5jsn61510ee174cf' \
     -s | jq '.results[].download' | sed 's/"//g')
 
-# potem zarzucic do wget 
-wget -b -c -i links.txt
+# wget -b -c -i links.txt
 # potem przeniesc
 rm -f wget-log*
-rm -f links.txt
+# rm -f links.txt
